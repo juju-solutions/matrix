371973bc98222d21f5b5a151418120ec75fac61e
Pete Vander Giessen <petevg@gmail.com>
2016-10-05 16:33:46 -0400

    Sketched out a more repeatable chaos function.
    Still need to check to see how config works, and actually get it to execute.

--- tests/chaos.py
+++ tests/chaos.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+from os import urandom
 
 async def chaos(context, rule):
     chaos_options = [
@@ -15,12 +16,25 @@ async def chaos(context, rule):
       "All the hippos go berserk"
     ]
     rule.log.info("Starting chaos")
-    for i in range(5):
-        kind = random.choice(chaos_options)
+
+    # Check to see if the plugin's config specifies a specific list of
+    # operations to perform.
+    operations = rule.config.chaos_ops # TODO: is this the right way
+                                       # to get the config for a rule?
+    if not operations:
+        # No operations specified; generate a list. Use a random seed
+        # if no seed is specified in the config.
+        seed = rule.config.chaos_seed or int.from_bytes(
+            urandom(4), byteorder='little')
+        rule.log.info('Running chaos with seed {}'.format(seed))
+        num_ops = rule.config.num_ops
+        operations = [random.choice(chaos_options) for i in range(num_ops)]
+
+    for op in operations:
         rule.log.debug("CHAOS: %s %s", kind, i)
         context.bus.dispatch(
                 origin="chaos",
-                payload=kind,
+                payload=op,
                 kind="chaos.activate"
                 )
         await asyncio.sleep(2, loop=context.loop)
