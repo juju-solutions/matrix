58089a14b8297456d816c9475d924966652e7d44
Pete Vander Giessen <petevg@gmail.com>
2016-10-24 13:39:55 -0400

    Fixed up tests.
    Replaced test_main.py with a proper matrix test plan. Uncommented and
    fixed the actions unit tests.

--- tests/glitch/test_actions.py
+++ tests/glitch/test_actions.py
@@ -1,17 +1,23 @@
 import unittest
 
-from matrix.plugins.glitch.actions import Actions, action
+from matrix.plugins.glitch.actions import _Actions
 
 
-#class TestAction(unittest.TestCase):
-#    def test_define(self):
-#        self.assertFalse('faux_action' in Actions)
+class FauxActions(_Actions): pass
 
-#        @action
-#        def faux_action(model, unit, **kwargs):
-#            pass
 
-#        self.assertTrue('faux_action' in Actions)
+class TestAction(unittest.TestCase):
+    def setUp(self):
+        self.actions = FauxActions()
+
+    def test_define(self):
+        self.assertFalse('faux_action' in self.actions)
+
+        @self.actions.decorate
+        def faux_action(model, unit, **kwargs):
+            pass
+
+        self.assertTrue('faux_action' in self.actions)
 
 if __name__ == '__main__':
     unittest.main()
