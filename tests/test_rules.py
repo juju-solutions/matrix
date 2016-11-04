80ec0b19ec6d190c861ed628a438c2880542d93e
Benjamin Saller <bcsaller@gmail.com>
2016-11-04 16:26:10 -0700

    bring tests into sync with rule file changes

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -31,13 +31,15 @@ def test_rule_conditions():
     assert t[2].has("while") is True
 
     assert t[0].match(context) is True
-    assert t[1].match(context) is True
+    assert t[1].match(context) is False
     assert t[2].match(context) is False
 
     context.states['chaos'] = "complete"
     context.states['test_traffic'] = "running"
     context.states['health.status'] = 'healthy'
     assert t[0].match(context) is True
+    # The until condition here is already met
+    # thus the rule won't match
     assert t[1].match(context) is False
     assert t[2].match(context) is True
     # The until condition here is already met
