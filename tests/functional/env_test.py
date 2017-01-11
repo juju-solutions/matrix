6482c6e835fd97354cfd270188b172d92da928f0
Pete Vander Giessen <petevg@gmail.com>
2017-01-11 09:56:11 -0500

    Added support for MATRIX_OUTPUT_DIR env variable.
    Helps us in cloud weather report, where we want to pass the value
    through multiple subprocess calls.

---
+++ tests/functional/env_test.py
@@ -0,0 +1,32 @@
+import subprocess
+from pathlib import Path
+import os
+
+from .harness import Harness
+
+
+class TestEnv(Harness):
+    '''
+    Verify that we can set config via env variables.
+
+    '''
+    def setUp(self):
+        super(TestEnv, self).setUp()
+
+        self.cmd = [
+            'matrix',
+            '-s', 'raw',
+            '-p', 'tests/basic_bundle',
+            '-D'
+        ]
+        os.environ['MATRIX_OUTPUT_DIR'] = self.tmpdir
+        self.artifacts = ['matrix.log']
+
+    def tearDown(self):
+        os.unsetenv('MATRIX_OUTPUT_DIR')
+
+    def test_set_env(self):
+        test = 'tests/test_non_gating.matrix'  # Lightweight test
+        proc = subprocess.run(self.cmd + [test], check=False, timeout=60)
+        self.assertEqual(proc.returncode, 0)
+        self.check_artifacts(2)  # Tarball and log
